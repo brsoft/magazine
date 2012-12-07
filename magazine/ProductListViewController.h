19fe03ca3163d34d5014707ef25f7ee50499e037
kenfeng <75649889@qq.com>
2012-12-08 00:12:17 +0800

    Revert "Revert "New feature product.""
    This reverts commit 07235ab63dc165e547813b9f19f6cb11a5c63814.

---
+++ magazine/ProductListViewController.h
@@ -0,0 +1,20 @@
+//
+//  ArticleListViewController.h
+//  magazine
+//
+//  Created by Leslie.Li on 11/22/12.
+//  Copyright (c) 2012 brsoft. All rights reserved.
+//
+
+#import "BaseViewController.h"
+#import "LoadingCellSingleton.h"
+#import "EGORefreshTableHeaderView.h"
+
+@interface ProductListViewController : BaseViewController <UITabBarControllerDelegate, UITableViewDataSource, UITableViewDelegate, EGORefreshTableHeaderDelegate>
+{
+  
+}
+
+
+
+@end
